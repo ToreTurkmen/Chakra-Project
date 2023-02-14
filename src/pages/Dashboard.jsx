ef37fd2ea9c4ce9223f09a7f577c0909cbe2e868
ToreTurkmen <=>
2023-02-14 23:39:30 +0100

    created new responsive style

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -6,7 +6,7 @@ export default function Dashboard() {
   return (
     <SimpleGrid p="10px" spacing={10} minChildWidth="250px">
       <Box bg="white" h="200px" border="1px solid">
-        <Text color={{base: 'black', md: 'blue', lg: 'red' }}>Tore</Text>
+        <Text color={{base: 'black', md: 'blue', lg: 'yellow ' }}>Tore</Text>
       </Box>
       <Box bg="white" h="200px" border="1px solid"></Box>
       <Box bg="white" h="200px" border="1px solid"></Box>
