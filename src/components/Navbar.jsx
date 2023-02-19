49dc98257dc068ced34df250c5436b00a54f65fd
ToreTurkmen <=>
2023-02-19 23:35:49 +0100

    created toast inside navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,23 @@
-import { Flex, Box, Heading, Text, Button, Spacer, HStack } from '@chakra-ui/react'
+import { UnlockIcon } from '@chakra-ui/icons'
+import { Flex, Box, Heading, Text, Button, Spacer, HStack, useToast } from '@chakra-ui/react'
 import React from 'react'
 
 export default function Navbar() {
+  const toast = useToast()
+
+  const showToast = () => {
+    toast({
+      title: 'Logged out',
+      description: 'Successfully logged out',
+      duration: 5000,
+      isClosable: true,
+      status: 'success',
+      position: 'top',
+      icon: <UnlockIcon />
+    })
+  }
+
+
   return (
     <Flex as="nav" p="10px" mb="40px" alignItems="center" >
       <Heading as="h1">Tore Turkmen</Heading>
@@ -11,7 +27,7 @@ export default function Navbar() {
       <HStack spacing="20px">
         <Box bg="gray.200" p="10px">T</Box>
         <Text>...@mail.com</Text>
-        <Button colorScheme="blue">Logout</Button>
+        <Button colorScheme="blue" onClick={showToast}>Logout</Button>
       </HStack>
       
     </Flex>
